7ac8999c1ad3834195fee68cad8f3109f21ae88c
Sam Shore <samjshore@gmail.com>
2021-01-03 18:13:49 -0500

    Added Splitbee Analytics

--- src/components/Providers/SEO.tsx
+++ src/components/Providers/SEO.tsx
@@ -50,6 +50,7 @@ export default function SEO() {
           rel="icon"
           href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
         />
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
       </Head>
     </React.Fragment>
   )
