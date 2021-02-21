913ce34c9e7ff34d191e90512ac32389a159bc78
Sam Shore <samjshore@gmail.com>
2021-02-21 16:15:39 -0500

    Make things pretty

--- src/components/PostDetailsGrid/index.tsx
+++ src/components/PostDetailsGrid/index.tsx
@@ -20,10 +20,13 @@ export default function PostDetailsGrid({ posts }) {
                           as={`/notebook/${slug.current}`}
                           passHref
                         >
-                          <a className="text-gray-900 dark:text-white font-mono text-xl">{title}</a>
+                          <a className="text-gray-900 dark:text-white font-mono text-xl bg-blue-100 dark:bg-blue-900 bg-opacity-50 mr-auto">{title}</a>
                         </Link>
                         {description && <p className="clamp-2 dark:text-gray-400 text-gray-600 font-light">{description}</p>}
-                          <p className="dark:text-gray-600 text-gray-400 text-sm">{new Date(_updatedAt).toDateString()}</p>
+                          <div className={"flex flex-row space-x-2 items-center"}>
+                          <svg className="w-4 h-4 dark:text-gray-600 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                          <p className="dark:text-gray-600 text-gray-400 text-sm font-mono font-thin tracking-tight">{new Date(_updatedAt).toDateString()}</p>
+                          </div>
                       </div>
               </div>
             )
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -13,23 +13,19 @@ function urlFor(source) {
 export default function Projects({ projects }) {  
     if (!projects || projects.length === 0) return null
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 grid-flow-row gap-12">
+        <div className="grid grid-cols-1 sm:grid-cols-3 grid-flow-row gap-12  sm:-mx-24">
         {projects.map(
             ({ _id, title = '',  description = '', color = '', status = '', url = '', mainImage = '' }) =>
                 <div key={_id}>
-                            <div className="flex flex-row items-center space-x-4">
-                                <div>
-                                 <img src={urlFor(mainImage).url()} className="w-16" />
-                                </div>
-                                <div className="flex flex-col space-y-1">
-                                    <div className="flex flex-row items-center text-gray-900 space-x-2 dark:text-white">
-                                        <h2 className="text-2xl font-mono">{title}</h2>
-                                        <div className={`px-2 py-1 bg-blue-500 bg-opacity-20 text-xs font-bold text-blue-500 rounded-full`}>{status}</div>
+                                
+                                <div className="flex flex-col justify-center items-stretch space-y-4">
+                                    <img src={urlFor(mainImage).url()} className="w-16 mx-auto" />
+                                    <div className={"flex flex-col space-y-1"}>
+                                        <h2 className="text-2xl font-mono text-gray-900 dark:text-white mx-auto">{title}</h2>
+                                        <p className="clamp-2 dark:text-gray-400 text-gray-600 font-light text-center">{description}</p>
                                     </div>
-                                    <p className="clamp-2 dark:text-gray-400 text-gray-600 font-light">{description}</p>
-                                    <a href={url} target="_blank" className={`text-base text-blue-500`}>Check it out</a>
+                                    <a href={url} target="_blank" className={`py-3 px-4 bg-gray-100 mr-auto text-xs text-gray-800 dark:bg-gray-800 dark:text-gray-200 font-mono mx-auto`}>View Project</a>
                                 </div>
-                            </div>
                         </div>
           )}
     
